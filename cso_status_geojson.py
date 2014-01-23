597c3b263128f231cb9ee209aa39bb92b7d5c4ea
keum <keum.peter@gmail.com>
2014-01-22 20:32:53 -0800

    need as working code

---
+++ cso_status_geojson.py
@@ -0,0 +1,13 @@
+import sys
+import csv
+import urllib2
+import pprint
+
+"""
+Take status csv file from url and coordinate csv file to create 
+data dictionary format and convert that format into geojson type. 
+This geojson data can then be loaded into GitHub page to view
+using it's mapping component
+
+"""
+
