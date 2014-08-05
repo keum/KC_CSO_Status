e30f6523f4a1ee5dcb86b4f17c2b1acf09e1a646
keum <keum.peter@gmail.com>
2014-08-05 13:47:12 -0700

    'update'

--- cso_status_geojson.py
+++ cso_status_geojson.py
@@ -142,10 +142,10 @@ for line in cso_status_csv:
     #Test to see record is  Seattle CSO data or not
     if line[0][0:5]=="NPDES": # this indicates the data is Seattle CSO
         cso_name = line[0]
-        cso_symbol = "circle"
+        cso_symbol = 'circle-stroked'
     else: #this is not  Seattle CSO
         cso_name = line[0][0:len(line[0])-12] #this is for king county cso
-        cso_symbol = "marker"
+        cso_symbol = 'Star Solid'
     #for all records
     CSO_Status = line[1]
     # If CSO exists, add to it.
